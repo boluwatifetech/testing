15702882a172126053f6a2dd9d317076d0825f07
boluwatifetech <boluabiola202@gmail.com>
2022-11-09 13:02:07 -0800

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import  Twitter from './tweet';
+
+function App(){  
+  return(
+    <div>
+      <h1>react begin in full option</h1>
+      <Twitter />
+    </div>
+
+  );
+}
+export default App;
---
+++ src/tweet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+function Twitter(){
+return(
+    <div className='tweet'>
+        <h1>this is like a twitter</h1>
+        <p>this is where it is done</p>
+        <h3>i kindly love twitter</h3>
+    </div>
+);
+}
+export default Twitter;
+
